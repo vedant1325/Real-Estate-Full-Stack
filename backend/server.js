1505512f50bec0c88e0325b981353dd07532f3f3
vedant1325 <147478924+vedant1325@users.noreply.github.com>
2025-01-01 14:28:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cookieParser());
-const allowedOrigins=['http://localhost:5173','http://localhost:5174']
+const allowedOrigins=['https://real-estate-frontend-bfpz.onrender.com/','https://real-estate-admin.onrender.com/']
 app.use(cors({
     origin: (origin, callback) => {
         if (allowedOrigins.includes(origin) || !origin) {
